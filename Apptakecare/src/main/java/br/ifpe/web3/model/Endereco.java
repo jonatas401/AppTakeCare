3dff47dba8d694bf2118e0ad8ac3b03aac41619f
jonatas401 <jonnatas56@hotmail.com>
2022-10-25 22:21:27 -0300

    ajustando codigo

---
+++ Apptakecare/src/main/java/br/ifpe/web3/model/Endereco.java
@@ -0,0 +1,55 @@
+package br.ifpe.web3.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+
+	
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String numeroResidencia;
+	
+	
+	
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getnumeroResidencia() {
+		return numeroResidencia;
+	}
+	public void setnumeroResidencia(String numeroresidencia) {
+		this.numeroResidencia = numeroresidencia;
+	}
+	
+}
---
+++ Apptakecare/src/main/java/br/ifpe/web3/model/TipoEmpresa.java
@@ -0,0 +1,9 @@
+package br.ifpe.web3.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public enum TipoEmpresa {
+	Salao, Barbearia, Estudio
+
+}
